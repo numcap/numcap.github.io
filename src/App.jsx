6c022a97bec991c1a1c05db1ff86730393a81752
Jonathan <juno9593@gmail.com>
2025-01-06 12:09:18 -0500

    Add Vercel Analytics and Speed Insights to improve performance tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Content } from "./layout/Content";
 import { Footer } from "./layout/Footer";
 import { Navbar } from "./layout/Navbar";
-// import { Navbar, Footer } from "react-club-components"
 
 function App() {
 	return (
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./output.css";
 import App from "./App.jsx";
 import { PositionProvider } from "./contexts";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react"
+
 
 createRoot(document.getElementById("root")).render(
 	<PositionProvider>
 		<StrictMode>
+			<Analytics />
+			<SpeedInsights />
 			<App />
 		</StrictMode>
 	</PositionProvider>
