9a6da248fffbb5b943f862bab199124fb322d7f0
Jonathan <juno9593@gmail.com>
2025-01-06 11:06:16 -0500

    Update Vite configuration and package scripts for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  // resolve: {
-  //   alias: {
-  //     '@images': '/src/images',
-  //     '@assets': '/src/assets',
-  //   }
-  // }
+  base: '/"numcap.github.io',
 })
